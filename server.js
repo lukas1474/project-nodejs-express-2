f29659fb9b5fbc6a2c5e146cc2303c0cd0c8bce9
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-17 17:49:06 +0200

    init handlebars in the project

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
     res.show = (name) => {
@@ -16,7 +19,7 @@ app.use('/user', (req, res, next) => {
 
 app.get('/public/404.png', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/404.png'));
-  });
+});
 
 app.get('/', (req, res) => {
     res.show('home.html');
@@ -26,9 +29,13 @@ app.get('/about', (req, res) => {
     res.show('about.html');
 });
 
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { layout: false, name: req.params.name });
+});
+
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, '/views/404.html'));
-  })
+})
 
 app.listen(8080, () => {
     console.log('Server is running on port: 8080');
