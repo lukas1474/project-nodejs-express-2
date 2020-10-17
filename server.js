8cae3f92647e3d895724ff8ad74d25a6050bf1f4
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-10-17 19:17:33 +0200

    all server files move from html to handlebars

--- server.js
+++ server.js
@@ -3,38 +3,49 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-app.engine('hbs', hbs());
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-        res.sendFile(path.join(__dirname, `/views/${name}`));
-    };
-    next();
-});
+// app.use('/user', (req, res, next) => {
+//     res.sendFile(path.join(__dirname, '/views/forbidden.html'));
+// });
 
-app.use('/user', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '/views/forbidden.html'));
-});
+// app.get('/public/404.png', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/404.png'));
+// });
 
-app.get('/public/404.png', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/404.png'));
-});
+// app.use((req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/404.html'));
+// })
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.show('home.html');
-});
-
-app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('home');
 });
 
 app.get('/hello/:name', (req, res) => {
-    res.render('hello', { layout: false, name: req.params.name });
+    res.render('hello', { name: req.params.name });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+    res.render('info');
+});
+
+app.get('/history', (req, res) => {
+    res.render('history');
 });
 
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '/views/404.html'));
+    res.status(404).send('404 not found...');
 })
 
 app.listen(8080, () => {
